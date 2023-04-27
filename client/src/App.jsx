7cf0f7e42c66f081e046c50dd512213eab5395f2
PKHWagner <se.aksnow@icloud.com>
2023-04-26 22:08:41 -0800

    Added a little functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,54 +3,59 @@ import React, { useEffect, useState } from 'react';
 
 function App() {
 
-  // const [allPokemon, setAllPokemon] = useState([]);
+  const [allPokemon, setAllPokemon] = useState([]);
 
-  // const submitHandler = (e) => {
-  //   e.preventDefault()
-  // fetch("https://pokeapi.co/api/v2/pokemon")
-  //   .then(response => {
-  //     return response.json();
-  //   }).then(response => {
-  //     console.log(response);
-  //   }).catch(err => {
-  //     console.log(err);
-  //   });
-  // setAllPokemon([...allPokemon, Response]);
-  // console.log(allPokemon);
-  const [pokemon, setPokemon] = useState([]);
-  useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon/?limit=40")
-      .then(response => response.json())
-      .then(response => setPokemon(response.results))
-  }, []);
+  const submitHandler = (e) => {
+    e.preventDefault()
+    fetch("https://pokeapi.co/api/v2/pokemon")
+      .then(response => {
+        return response.json();
+      }).then(response => {
+        console.log(response);
+        setAllPokemon(response.results);
+      }).catch(err => {
+        console.log(err);
+      });
+
+    console.log(allPokemon);
+  }
+  // const [pokemon, setPokemon] = useState([]);
+  // useEffect(() => {
+  //   fetch("https://pokeapi.co/api/v2/pokemon/?limit=40")
+  //     .then(response => response.json())
+  //     .then(response => setPokemon(response.results))
+  // }, []);
 
 
 
   return (
     <div className="App">
-      <form action="">
+      <form action="" onSubmit={submitHandler}>
         <button>Fetch All Pokemon</button>
       </form>
 
       <hr />
 
       <h1>Your List of Pokemon</h1>
-      <div>
+      {/* <div>
         {
           pokemon.length > 0 && pokemon.map((pokemon, index) => {
             return (<div key={index}>{pokemon.name}</div>)
           })
         }
+      </div> */}
+      <div>
+        {
+          allPokemon.map((pokemon, i) => (
+            <div key={i}>
+              <ul>
+                <li>{pokemon.name}</li>
+              </ul>
+            </div >
+          ))
+        }
       </div>
-      {/* {
-        allPokemon.map((pokemon, i) => (
-          <div key={i}>
-            <ul>
-              <li>{pokemon.name}</li>
-            </ul>
-          </div >
-        ))
-      } */}
+
     </div>
   );
 }
